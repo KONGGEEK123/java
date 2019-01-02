c6cf04be1957766811b5ca371aa586ee9b453c0c
WangZhen_iOS <yazhen.wang@krvision.cn>
2019-01-02 14:51:27 +0800

    轮询接口

--- TimingTask/src/main/java/com/example/demo/DemoApplication.java
+++ TimingTask/src/main/java/com/example/demo/DemoApplication.java
@@ -31,8 +31,8 @@ public class DemoApplication {
 
     public static void timer() {
         Calendar calendar = Calendar.getInstance();
-        calendar.set(Calendar.HOUR_OF_DAY, 14); // 控制时
-        calendar.set(Calendar.MINUTE, 37);    // 控制分
+        calendar.set(Calendar.HOUR_OF_DAY, 15); // 控制时
+        calendar.set(Calendar.MINUTE, 0);    // 控制分
         calendar.set(Calendar.SECOND, 0);    // 控制秒
 
         Date time = calendar.getTime();     // 得出执行任务的时间,此处为今天的12：00：00
@@ -40,7 +40,7 @@ public class DemoApplication {
         timer.scheduleAtFixedRate(new TimerTask() {
             public void run() {
                 System.out.println("-------设定要指定任务--------");
-//                timingTaskMethod();
+                timingTaskMethod();
             }
             }, time, 1000 * 60/* * 60 * 24*/);// 这里设定将延时每天固定执行
         }
