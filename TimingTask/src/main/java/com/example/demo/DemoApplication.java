37037e939cbe7c6e2855f0b57a72a8b675dd58a6
WangZhen_iOS <yazhen.wang@krvision.cn>
2019-01-02 10:30:34 +0800

    commit

---
+++ TimingTask/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,73 @@
+package com.example.demo;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.log.LogFormatUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import sun.net.www.http.HttpClient;
+import sun.rmi.runtime.Log;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("task")
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+        timer();
+    }
+
+    public static void timer() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, 8); // 控制时
+        calendar.set(Calendar.MINUTE, 0);    // 控制分
+        calendar.set(Calendar.SECOND, 0);    // 控制秒
+
+        Date time = calendar.getTime();     // 得出执行任务的时间,此处为今天的12：00：00
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            public void run() {
+                System.out.println("-------设定要指定任务--------");
+                timingTaskMethod();
+            }
+            }, time, 1000 * 60 * 60 * 24);// 这里设定将延时每天固定执行
+        }
+    public static String WEBHOOK_TOKEN = "https://oapi.dingtalk.com/robot/send?access_token=3902e88c20e9ca33f8bd0a13732922fe375d6d8e5c42f34c7b62c32d49c52574";
+    public static void timingTaskMethod() {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpPost httppost = new HttpPost(WEBHOOK_TOKEN);
+        httppost.addHeader("Content-Type", "application/json; charset=utf-8");
+        String textMsg = "{ \"msgtype\": \"text\", \"text\": {\"content\": \"新的一天，新的开始！吖咪提醒您记得及时提交日志！\"}}";
+        StringEntity se = new StringEntity(textMsg, "utf-8");
+        httppost.setEntity(se);
+        try {
+            HttpResponse response = httpclient.execute(httppost);
+            if (response.getStatusLine().getStatusCode()== HttpStatus.SC_OK){
+                String result= EntityUtils.toString(response.getEntity(), "utf-8");
+                System.out.println(result);
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+
+    }
+
+    @RequestMapping("sendkeyword")
+    public String sendKeyword() {
+        return "你调用了发送关键字的接口";
+    }
+}
+
+
