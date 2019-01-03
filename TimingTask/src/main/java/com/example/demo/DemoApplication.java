6d7b8ac8501d1bcc1833d827b33060dfaf6ded49
WangZhen_iOS <yazhen.wang@krvision.cn>
2019-01-03 08:35:43 +0800

    时间

--- TimingTask/src/main/java/com/example/demo/DemoApplication.java
+++ TimingTask/src/main/java/com/example/demo/DemoApplication.java
@@ -31,7 +31,7 @@ public class DemoApplication {
     public void timer() {
         Calendar calendar = Calendar.getInstance();
         calendar.set(Calendar.HOUR_OF_DAY, 8); // 控制时
-        calendar.set(Calendar.MINUTE, 45);    // 控制分
+        calendar.set(Calendar.MINUTE, 40);    // 控制分
         calendar.set(Calendar.SECOND, 0);    // 控制秒
 
         Date time = calendar.getTime();     // 得出执行任务的时间,此处为今天的12：00：00
